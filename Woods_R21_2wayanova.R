d0d6e1a1a07222295607a80c68ff36611f5ac47b
Philip Woods <19PWoods@gmail.com>
2023-04-25 16:22:02 -0400

    got rid of unnecessary packages

--- Woods_R21_2wayanova.R
+++ Woods_R21_2wayanova.R
@@ -1,13 +1,11 @@
 library(tidyverse)
 library(readxl)
-library(emmeans)
-library(multcomp)
-library(lmerTest)
+
 
 # setwd("C:/Users/Phil/Dropbox/MBL/Tension + AaBbCc/R21-MAT")
 setwd("C:/Users/pcw00/Dropbox/University of Massachusetts Amherst/MBL/Tension + AaBbCc/R21-MAT")
 
-my_data <- read_excel("R21-Tension+AkBbCc__PW_R_4-3-23.xlsx",
+my_data <- read_excel("R21-Tension+AkBbCc__PW_R_4-25-23.xlsx",
                       sheet = "Final - Run only") %>% 
   mutate(AgexWeight = as.factor(AgexWeight)) %>% 
   mutate(AgexWeightxSex = as.factor(AgexWeightxSex))
