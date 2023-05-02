34cd0dd02ff387c4302be118b05ed9171c11a119
Philip Woods <19PWoods@gmail.com>
2023-05-02 11:03:17 -0400

    added two way anova analysis for R21

--- Woods_R21_2wayanova.R
+++ Woods_R21_2wayanova.R
@@ -23,61 +23,106 @@ my_data_IIAX <- my_data %>%
 
 ### MHC I Analysis--------------------------------------------------------------------------------
 
-I_Ten_aov <- aov(Tension ~ Sex*Wt.Status, data = my_data_I)
+I_Ten_aov <- aov(Tension ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_Ten_aov)
-I_Ten2_aov <- aov(Tension ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
-summary(I_Ten2_aov)
-I_Ten3_aov <- aov(Tension ~ Wt.Status*Sex, data = my_data_I)
-summary(I_Ten3_aov)
 
-I_Force_aov <- aov(Force ~Sex*Wt.Status, data = my_data_I)
+I_Force_aov <- aov(Force ~Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_Force_aov)
-I_Force2_aov <- aov(Force ~Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_Force2_aov)
 
-I_CSA_aov <- aov(CSA ~ Sex*Wt.Status, data = my_data_I)
+I_CSA_aov <- aov(CSA ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_CSA_aov)
-I_CSA2_aov <- aov(CSA ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_CSA2_aov)
 
-I_A_aov <- aov(A ~ Sex*Wt.Status, data = my_data_I)
+I_A_aov <- aov(A ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_A_aov)
-I_A2_aov <- aov(A ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_A2_aov)
 
-I_k_aov <- aov(k ~ Sex*Wt.Status, data = my_data_I)
+I_k_aov <- aov(k ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_k_aov)
-I_k2_aov <- aov(k ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_k2_aov)
 
-I_B_aov <- aov(B ~ Sex*Wt.Status, data = my_data_I)
+I_B_aov <- aov(B ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_B_aov)
-I_B2_aov <- aov(B ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_B2_aov)
 
-I_C_aov <- aov(C ~ Sex*Wt.Status, data = my_data_I)
+I_C_aov <- aov(C ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_C_aov)
-I_C2_aov <- aov(C ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_C2_aov)
 
-I_lb_aov <- aov(lb ~ Sex*Wt.Status, data = my_data_I)
+I_lb_aov <- aov(lb ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_lb_aov)
-I_lb2_aov <- aov(lb ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_lb2_aov)
 
-I_2pib_aov <- aov(twopib ~ Sex*Wt.Status, data = my_data_I)
+I_2pib_aov <- aov(twopib ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_2pib_aov)
-I_2pib2_aov <- aov(twopib ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_2pib2_aov)
 
-I_lc_aov <- aov(lc ~ Sex*Wt.Status, data = my_data_I)
+I_lc_aov <- aov(lc ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_lc_aov)
-I_lc2_aov <- aov(lc ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_lc2_aov)
 
-I_ton_aov <- aov(ton ~ Sex*Wt.Status, data = my_data_I)
+I_ton_aov <- aov(ton ~ Sex*Wt.Status + Error(SubjNum), data = my_data_I)
 summary(I_ton_aov)
-I_ton2_aov <- aov(ton ~ Sex*Wt.Status + (1|SubjNum), data = my_data_I)
-summary(I_ton2_aov)
+
+### MHC IIA --------------------------------------------------------------------------------
+
+IIA_Ten_aov <- aov(Tension ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_Ten_aov)
+
+IIA_Force_aov <- aov(Force ~Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_Force_aov)
+
+IIA_CSA_aov <- aov(CSA ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_CSA_aov)
+
+IIA_A_aov <- aov(A ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_A_aov)
+
+IIA_k_aov <- aov(k ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_k_aov)
+
+IIA_B_aov <- aov(B ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_B_aov)
+
+IIA_C_aov <- aov(C ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_C_aov)
+
+IIA_lb_aov <- aov(lb ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_lb_aov)
+
+IIA_2pib_aov <- aov(twopib ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_2pib_aov)
+
+IIA_lc_aov <- aov(lc ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_lc_aov)
+
+IIA_ton_aov <- aov(ton ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIA)
+summary(IIA_ton_aov)
 
 
+### MHCA IIAX ---------------------------------------------------------------------------------
+
+IIAX_Ten_aov <- aov(Tension ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_Ten_aov)
+
+IIAX_Force_aov <- aov(Force ~Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_Force_aov)
+
+IIAX_CSA_aov <- aov(CSA ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_CSA_aov)
+
+IIAX_A_aov <- aov(A ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_A_aov)
+
+IIAX_k_aov <- aov(k ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_k_aov)
+
+IIAX_B_aov <- aov(B ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_B_aov)
+
+IIAX_C_aov <- aov(C ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_C_aov)
+
+IIAX_lb_aov <- aov(lb ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_lb_aov)
+
+IIAX_2pib_aov <- aov(twopib ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_2pib_aov)
+
+IIAX_lc_aov <- aov(lc ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_lc_aov)
+
+IIAX_ton_aov <- aov(ton ~ Sex*Wt.Status + Error(SubjNum), data = my_data_IIAX)
+summary(IIAX_ton_aov)
